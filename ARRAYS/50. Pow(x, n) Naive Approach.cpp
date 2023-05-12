2ce54e6432e7f75aed1d76211ed79f0cc2776fbc
Pragna Biswas <97850804+Coder-pipo@users.noreply.github.com>
2023-05-13 02:38:10 +0530

    Update 50. Pow(x, n) Naive Approach.cpp

--- ARRAYS/50. Pow(x, n) Naive Approach.cpp
+++ ARRAYS/50. Pow(x, n) Naive Approach.cpp
@@ -1,8 +1,20 @@
+//But it won't work as the given range of n is -2^31 to 2^31
+
 class Solution {
 public:
     double myPow(double x, int n) {
-        double val;
-        val=pow(x,n);
+        double val=1;
+        if(n<0){
+            n=-1*n;
+            for(int i=0;i<n;i++)
+                val=val*x;
+            
+            val=1/val;
+            return val;
+        }
+
+        for(int i=0;i<n;i++)
+            val=val*x;
 
         return val;
     }
