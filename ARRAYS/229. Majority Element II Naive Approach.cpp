27f8b0b735bc49b4301752033597e6866d64052b
Pragna Biswas <97850804+Coder-pipo@users.noreply.github.com>
2023-05-14 23:21:21 +0530

    Create 229. Majority Element II Naive Approach.cpp

---
+++ ARRAYS/229. Majority Element II Naive Approach.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+
+        vector<int> v;
+        int maximum;
+        int val;
+        for(auto it:mp){
+            if(it.second>(nums.size()/3)){
+                v.push_back(it.first);
+            }
+        }
+        return v;
+    }
+};
