332606af12d4ecbaa8ade6de6383c500b8905913
Pragna Biswas <97850804+Coder-pipo@users.noreply.github.com>
2023-05-15 23:19:35 +0530

    Update 1. Two Sum Naive Approach.cpp

--- ARRAYS/1. Two Sum Naive Approach.cpp
+++ ARRAYS/1. Two Sum Naive Approach.cpp
@@ -4,8 +4,8 @@ public:
         vector<int> ans;
         bool flag=0;
         for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(i!=j&&nums[i]+nums[j]==target){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
                     ans.push_back(i);
                     ans.push_back(j);
                     flag=1;
