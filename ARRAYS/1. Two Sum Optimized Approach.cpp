0217ee39a10e855d1862e12b7042620ce0a32459
Pragna Biswas <97850804+Coder-pipo@users.noreply.github.com>
2023-05-20 02:41:07 +0530

    Update and rename 1. Two Sum Better Approach.cpp to 1. Two Sum Optimized Approach.cpp

--- ARRAYS/1. Two Sum Optimized Approach.cpp
+++ ARRAYS/1. Two Sum Optimized Approach.cpp
@@ -1,4 +1,4 @@
-//O(N)
+//In the worst case(which rarely happens), the unordered_map takes O(N) to find an element. In that case, the time complexity will be O(N^2). If we use map instead of unordered_map, the time complexity will be O(N* logN) as the map data structure takes logN time to find an element.
 
 class Solution {
 public:
