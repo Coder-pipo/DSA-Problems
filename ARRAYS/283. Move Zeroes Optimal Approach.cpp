b2761e714e3786e993b2710c8392ab498e087a09
Pragna Biswas <97850804+Coder-pipo@users.noreply.github.com>
2023-05-23 23:18:54 +0530

    Create 283. Move Zeroes Optimal Approach.cpp

---
+++ ARRAYS/283. Move Zeroes Optimal Approach.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()){
+            if(nums[i]==0)
+                break;
+            else
+                i++;
+        }
+
+        int j=i;
+        int k=i+1;
+        while(j<nums.size()&&k<nums.size()){
+            if(nums[k]!=0){
+                swap(nums[j],nums[k]);
+                j++;
+            }
+            else
+                k++;
+        }
+    }
+};
