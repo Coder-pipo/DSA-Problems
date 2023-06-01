c0e03611948d0437ea407c6a34dd6688f6678468
Pragna Biswas <97850804+Coder-pipo@users.noreply.github.com>
2023-06-01 23:28:16 +0530

    Create 35. Search Insert Position.cpp

---
+++ Binary Search/35. Search Insert Position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int lb=0;
+        int ub=nums.size();
+
+        if(target>nums[ub-1])
+            return ub;
+
+        while(lb<=ub){
+            int mid=lb+(ub-lb)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+
+            else if(target<nums[mid])
+                ub=mid-1;
+
+            else
+                lb=mid+1;
+        }
+
+        return lb;
+    }
+};
