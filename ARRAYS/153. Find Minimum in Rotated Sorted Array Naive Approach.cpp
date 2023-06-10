d9fda5936dcd1d7f1498379da032a873b1857776
Pragna Biswas <97850804+Coder-pipo@users.noreply.github.com>
2023-06-11 01:58:00 +0530

    Create 153. Find Minimum in Rotated Sorted Array Naive Approach.cpp

---
+++ ARRAYS/153. Find Minimum in Rotated Sorted Array Naive Approach.cpp
@@ -0,0 +1,13 @@
+//O(N)
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int res=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            res=min(res,nums[i]);
+        }
+
+        return res;
+    }
+};
